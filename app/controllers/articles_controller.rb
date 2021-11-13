bf0743b6b540772fec0eebe5b6d4bf71984aef60
derikbf <derikb@gmail.com>
2021-11-12 21:01:55 -0300

    Created create Article New and params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,5 +9,25 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
  
+  # Article/Create
+  def new
+    @article = Article.new
+  end
+ 
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else 
+      render :new
+    end
+  end
+
+  private 
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-
-  
+  validates :title, presence: true, length: {minimum:5, maximum:80}
+  validates :body, presence: true, length: {minimum:10} 
 end
